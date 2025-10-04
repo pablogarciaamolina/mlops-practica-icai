68d10ef37c8afdd5525f803b7d1650d2991a586c
pablogarciaamolina <pablogarciamolina6@gmail.com>
2025-10-04 17:11:23 +0200

    Modificación para usar el fichero de datos de entrenamiento almacenado en DVC remoto.

--- train.py
+++ train.py
@@ -1,19 +1,27 @@
+import os
 import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 import joblib
 import mlflow
 import mlflow.sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 import dagshub
 dagshub.init(repo_owner='pablogarciaamolina', repo_name='mlops-practica-icai', mlflow=True)
 
-# Cargar el conjunto de datos
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+# Cargar el conjunto de datos desde el archivo CSV
+try:
+    iris = pd.read_csv('data/iris_dataset.csv')
+except FileNotFoundError:
+    print("Error: El archivo 'data/iris_dataset.csv' no fue encontrado.")
+
+# Dividir el DataFrame en características (X) y etiquetas (y)
+X = iris.drop('target', axis=1)
+y = iris['target']
 
 # Iniciar un experimento de MLflow
 with mlflow.start_run():
@@ -23,7 +31,7 @@ with mlflow.start_run():
     )
 
     # Inicializar y entrenar el modelo
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = RandomForestClassifier(n_estimators=200, random_state=42)
     model.fit(X_train, y_train)
     
     # Realizar predicciones y calcular la precisión
@@ -37,7 +45,7 @@ with mlflow.start_run():
     mlflow.sklearn.log_model(model, "random-forest-model")
     
     # Registrar parámetros y métricas
-    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("n_estimators", 200)
     mlflow.log_metric("accuracy", accuracy)
     print(f"Modelo entrenado y precisión: {accuracy:.4f}")
     print("Experimento registrado con MLflow.")
