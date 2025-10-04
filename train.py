0be8aa006a278ef926d8d7fc350d286c8d473974
pablogarciaamolina <pablogarciamolina6@gmail.com>
2025-10-04 17:42:23 +0200

    Nuevo codigo de train para el uso de CML

--- train.py
+++ train.py
@@ -10,8 +10,8 @@ import mlflow.sklearn
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import dagshub
-dagshub.init(repo_owner='pablogarciaamolina', repo_name='mlops-practica-icai', mlflow=True)
+tracking_uri = os.environ.get("MLFLOW_TRACKING_URI")
+mlflow.set_tracking_uri(tracking_uri)
 
 # Cargar el conjunto de datos desde el archivo CSV
 try:
@@ -49,3 +49,15 @@ with mlflow.start_run():
     mlflow.log_metric("accuracy", accuracy)
     print(f"Modelo entrenado y precisión: {accuracy:.4f}")
     print("Experimento registrado con MLflow.")
+
+    # --- Sección de Reporte para CML ---
+    # 1. Generar la matriz de confusión
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.title('Matriz de Confusión')
+    plt.xlabel('Predicciones')
+    plt.ylabel('Valores Reales')
+    plt.savefig('results/confusion_matrix.png')
+    print("Matriz de confusión guardada como 'confusion_matrix.png'")
+    # --- Fin de la sección de Reporte ---
